fd390cd15289d84948132c5fdc67dfcf74353de1
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-05-15 00:21:03 +0530

    feat: Navigation Drawe initial commit

--- app/src/main/java/com/slp/songwiki/ui/activity/SongWikiActivity.java
+++ app/src/main/java/com/slp/songwiki/ui/activity/SongWikiActivity.java
@@ -1,61 +1,70 @@
 package com.slp.songwiki.ui.activity;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-   
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-  
-import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.slp.songwiki.R;
-import com.slp.songwiki.adapter.ArtistAdapter;
 import com.slp.songwiki.adapter.SongWikiPagerAdapter;
-import com.slp.songwiki.utilities.LastFmUtils;
-import com.slp.songwiki.utilities.NetworkUtils;
 import com.slp.songwiki.utilities.PreferenceUtils;
 
-public class SongWikiActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SongWikiActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
 
     private SongWikiPagerAdapter pagerAdapter;
     private ViewPager mViewPager;
-    private AdView adView;
+    @Bind(R.id.banner_ad)
+    AdView adView;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.tabs)
+    TabLayout tabLayout;
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_song_wiki);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.song_wiki_main);
+        ButterKnife.bind(this);
+        setUpNavigationView();
         setSupportActionBar(toolbar);
-        pagerAdapter = new SongWikiPagerAdapter(getSupportFragmentManager(),this);
+        pagerAdapter = new SongWikiPagerAdapter(getSupportFragmentManager(), this);
 
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(pagerAdapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
         tabLayout.setupWithViewPager(mViewPager);
         adView = (AdView) findViewById(R.id.banner_ad);
-        Log.i( "Top artisst limit ", PreferenceUtils.getTopArtistsLimit(this));
+        Log.i("Top artist limit ", PreferenceUtils.getTopArtistsLimit(this));
         showBannerAd();
 
+    }
+
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
     }
 
     private void showBannerAd() {
@@ -77,17 +86,22 @@ public class SongWikiActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.favourite_artists) {
-            startActivity(new Intent(this,FavouriteArtistActivity.class));
+            startActivity(new Intent(this, FavouriteArtistActivity.class));
             return true;
-        }else if(id == R.id.song_wiki_settings){
-            startActivity(new Intent(this,SettingsActivity.class));
+        } else if (id == R.id.song_wiki_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_song_wiki,menu);
+        getMenuInflater().inflate(R.menu.menu_song_wiki, menu);
         return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
