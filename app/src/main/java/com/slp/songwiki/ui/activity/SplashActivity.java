9fbb893b7fb7f5d45c06287cdb82032dad9746cc
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-05-27 18:16:32 +0530

    feat: added splash screen

---
+++ app/src/main/java/com/slp/songwiki/ui/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.slp.songwiki.ui.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.slp.songwiki.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showSplashScreen();
+
+    }
+
+    private void showSplashScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,SongWikiActivity.class));
+                finish();
+            }
+        },SPASH_TIME_OUT);
+    }
+}
