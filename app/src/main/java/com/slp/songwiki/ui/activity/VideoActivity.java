c2d06a243d2ab4d075d5464fcf878f6855fe2e41
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-06-25 02:07:27 +0530

    feat: video player initial commit
     added youtube dependency

---
+++ app/src/main/java/com/slp/songwiki/ui/activity/VideoActivity.java
@@ -0,0 +1,15 @@
+package com.slp.songwiki.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.slp.songwiki.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
